a998322d2fcb060e2a3f013c2ee9cde6ea939c44
harshpratapsingh2147 <harsh.singh@visionias.in>
2024-07-02 14:39:37 +0530

    updated gunicorn setup file and req.txt

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,12 @@
-workers = 4
+workers = 3  # Adjust based on your CPU cores
 bind = "0.0.0.0:8002"
-worker_class = 'sync'  # Worker class
+worker_class = 'gevent'  # Consider using 'gevent' for I/O-bound applications
 worker_connections = 1000  # Maximum number of simultaneous clients
-timeout = 240
+timeout = 60
 
 reload = True
 accesslog = "/app/access.log"
 errorlog = "/app/error.log"
+
+max_requests = 10  # Force workers to restart after handling 1000 requests
+max_requests_jitter = 0  # Add randomness to max_requests to avoid simultaneous restarts
